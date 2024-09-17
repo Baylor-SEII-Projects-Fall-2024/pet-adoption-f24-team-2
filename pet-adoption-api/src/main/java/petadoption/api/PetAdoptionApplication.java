fd5126b1aa686115199c5ab32f098bc4df94b899
wesley-goyette <wesley.goyette@twostory.com>
2024-09-17 12:36:02 -0500

    yeat

--- pet-adoption-api/src/main/java/petadoption/api/PetAdoptionApplication.java
+++ pet-adoption-api/src/main/java/petadoption/api/PetAdoptionApplication.java
@@ -3,17 +3,21 @@ package petadoption.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class PetAdoptionApplication {
     public static void main(String[] args) {
 
-        Dotenv dotenv = Dotenv.load();
+		File dotenvFile = new File(".env");
 
-        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		if (dotenvFile.exists()) {
+			Dotenv dotenv = Dotenv.load();
+			System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+			System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+			System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		}
 
         SpringApplication.run(PetAdoptionApplication.class, args);
     }
