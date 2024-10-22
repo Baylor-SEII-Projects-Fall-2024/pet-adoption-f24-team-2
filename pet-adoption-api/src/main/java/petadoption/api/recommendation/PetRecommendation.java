47cbc5cb9ff7d31c5b6d0a7662911eded0ab4935
Oliver Liu <oliverliu100@gmail.com>
2024-10-22 15:10:49 -0500

    Add Principal Component Analysis to Pet Recommendation

--- pet-adoption-api/src/main/java/petadoption/api/recommendation/PetRecommendation.java
+++ pet-adoption-api/src/main/java/petadoption/api/recommendation/PetRecommendation.java
@@ -3,6 +3,9 @@ package petadoption.api.recommendation;
 import petadoption.api.pet.Pet;
 import petadoption.api.user.User;
 import java.util.Arrays;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.EigenDecomposition;
+import org.apache.commons.math3.stat.correlation.Covariance;
 
 /*
 
@@ -20,60 +23,104 @@ public class PetRecommendation {
     static final double WEIGHT_AGE = 1.25;
     static final double WEIGHT_GENDER = 1.25;
 
+    // Number of principal components to use
+    private static final int NUM_PRINCIPAL_COMPONENTS = 3;
+
     // function to apply the above weights to adjust the importance of attributes
     public static double[] applyWeights(double[] attributes) {
         double[] temp = new double[petAttributes.getNumAttributes()];
         System.arraycopy(attributes, 0, temp, 0, petAttributes.getNumAttributes());
 
-        temp[0]*=WEIGHT_SPECIES;
-        temp[1]*=WEIGHT_SPECIES;
-        temp[2]*=WEIGHT_SPECIES;
-        temp[3]*=WEIGHT_COLOR;
-        temp[4]*=WEIGHT_COLOR;
-        temp[5]*=WEIGHT_COLOR;
-        temp[6]*=WEIGHT_GENDER;
-        temp[7]*=WEIGHT_GENDER;
-        temp[8]*=WEIGHT_AGE;
+        temp[0] *= WEIGHT_SPECIES;
+        temp[1] *= WEIGHT_SPECIES;
+        temp[2] *= WEIGHT_SPECIES;
+        temp[3] *= WEIGHT_COLOR;
+        temp[4] *= WEIGHT_COLOR;
+        temp[5] *= WEIGHT_COLOR;
+        temp[6] *= WEIGHT_GENDER;
+        temp[7] *= WEIGHT_GENDER;
+        temp[8] *= WEIGHT_AGE;
 
         return temp;
+    }
+
+    // Perform PCA on the attribute data
+    public static double[][] performPCA(double[][] data) {
+        // Center the data
+        double[] means = new double[data[0].length];
+        for (int i = 0; i < data[0].length; i++) {
+            double sum = 0;
+            for (double[] row : data) {
+                sum += row[i];
+            }
+            means[i] = sum / data.length;
+        }
+
+        for (double[] row : data) {
+            for (int i = 0; i < row.length; i++) {
+                row[i] -= means[i];
+            }
+        }
+
+        // Calculate covariance matrix
+        Covariance covariance = new Covariance(new Array2DRowRealMatrix(data));
+        double[][] covMatrix = covariance.getCovarianceMatrix().getData();
+
+        // Perform eigen decomposition
+        EigenDecomposition eigenDecomposition = new EigenDecomposition(new Array2DRowRealMatrix(covMatrix));
+
+        // Sort eigenvectors by eigenvalues in descending order
+        double[] eigenvalues = eigenDecomposition.getRealEigenvalues();
+        Integer[] indices = new Integer[eigenvalues.length];
+        for (int i = 0; i < indices.length; i++) {
+            indices[i] = i;
+        }
+        Arrays.sort(indices, (a, b) -> Double.compare(eigenvalues[b], eigenvalues[a]));
+
+        // Select top principal components
+        double[][] principalComponents = new double[NUM_PRINCIPAL_COMPONENTS][];
+        for (int i = 0; i < NUM_PRINCIPAL_COMPONENTS; i++) {
+            principalComponents[i] = eigenDecomposition.getEigenvector(indices[i]).toArray();
+        }
+
+        // Project data onto principal components
+        double[][] projectedData = new double[data.length][NUM_PRINCIPAL_COMPONENTS];
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < NUM_PRINCIPAL_COMPONENTS; j++) {
+                for (int k = 0; k < data[i].length; k++) {
+                    projectedData[i][j] += data[i][k] * principalComponents[j][k];
+                }
+            }
+        }
+
+        return projectedData;
     }
 
     // computes cosine similarity between two vectors (a user and pet attributes)
     public static double calcPetSimilarity(User u, Pet p) {
         // creates copies of arrays for manipulation
-        double[] temp1 = Arrays.copyOf(u.getAttributes().getAttributes(), u.getAttributes().getAttributes().length+1);
-        double[] temp2 = Arrays.copyOf(p.getAttributes().getAttributes(), p.getAttributes().getAttributes().length+1);
+        double[] temp1 = Arrays.copyOf(u.getAttributes().getAttributes(), u.getAttributes().getAttributes().length + 1);
+        double[] temp2 = Arrays.copyOf(p.getAttributes().getAttributes(), p.getAttributes().getAttributes().length + 1);
         temp2[8] = temp1[8] - Math.abs(temp2[8] - temp1[8]);
 
         // applies weights to the vectors
         double[] vectorA = applyWeights(temp1);
         double[] vectorB = applyWeights(temp2);
 
-        /*
+        // Combine vectors for PCA
+        double[][] combinedData = { vectorA, vectorB };
 
-        This is the actual calculation of the similarity.
-        It finds the angle between 2 vectors (which are the user/pet attribute arrays).
+        // Perform PCA
+        double[][] projectedData = performPCA(combinedData);
 
-        We want the magnitude of some values to matter because of the nature of the attributes, such as age. We want to
-        consider the magnitude of the age because it is a linear value. But we don't want to consider the magnitude of
-        a value such as species, because it isn't a linear value. We don't consider the distance between a cat and a dog
-        as any different from the distance between a cat and a rabbit.
-
-        The way that I have some values' magnitudes mattering and some not, is using one-hot encoding. This basically
-        means that for values where magnitude doesn't matter, we have them stored in multiple spaces in the array.
-        For example, species is stored in indices 0,1, and 2. If a pet is a cat, species is stored as [1][0][0], but a
-        dog would be stored as [0][1][0], and a rabbit would be [0][0][1]. This allows for species to still make a
-        difference in the return value, but have no difference in distance between different species.
-        For values where magnitude matters, we leave them as is, where their actual value is stored.
-
-         */
+        // Calculate similarity using projected data
         double dotProduct = 0.0;
         double normA = 0.0;
         double normB = 0.0;
-        for (int i = 0; i < vectorA.length; i++) {
-            dotProduct += vectorA[i] * vectorB[i];
-            normA += Math.pow(vectorA[i], 2);
-            normB += Math.pow(vectorB[i], 2);
+        for (int i = 0; i < projectedData[0].length; i++) {
+            dotProduct += projectedData[0][i] * projectedData[1][i];
+            normA += Math.pow(projectedData[0][i], 2);
+            normB += Math.pow(projectedData[1][i], 2);
         }
         return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
     }
