d95f0a955b1b1aab403f7d141c238517a999fb63
Carter Collins <carter_collins3@baylor.edu>
2024-12-10 11:02:54 -0600

    event display

--- pet-adoption-frontend/src/components/EventDisplay.js
+++ pet-adoption-frontend/src/components/EventDisplay.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, CardContent, Typography, Button, Grid2 } from "@mui/material";
-import { useState } from "react";
 import { request } from "@/axios_helper";
 import { getUserID } from "@/axios_helper";
 import { DateTimePicker } from "@mui/x-date-pickers";
@@ -20,6 +19,14 @@ function EventDisplayCard(props) {
   const [snackbarMessage, setSnackbarMessage] = useState("");
   const [snackbarSeverity, setSnackbarSeverity] = useState("");
   const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [selectedPetId, setSelectedPetId] = useState("");
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    request("GET", "/pets")
+        .then((response) => setPets(response.data))
+        .catch((error) => console.log(error));
+  }, []);
 
   function handleEdit() {
     setIsEditing(true);
@@ -28,9 +35,9 @@ function EventDisplayCard(props) {
   function cancelEdit() {
     setIsEditing(false);
     setName(event.name);
-    setDescription(event.description)
-    setLocation(event.location)
-    setDate(event.date)
+    setDescription(event.description);
+    setLocation(event.location);
+    setDate(event.date);
     setSnackbarMessage("Event edit canceled");
     setSnackbarSeverity("info");
     setSnackbarOpen(true);
@@ -38,21 +45,19 @@ function EventDisplayCard(props) {
 
   function handleRemove() {
     request("DELETE", `/events/${event.id}`, null)
-      .then((response) => {
-        if( response.status === 204 ) {
-          let index = currEvents.findIndex(e => e.id === event.id);
-          currEvents = [
-            ...currEvents.slice(0, index), // Elements before the one to delete
-            ...currEvents.slice(index + 1) // Elements after the one to delete
-          ]
-          setSnackbarMessage("Event deleted!");
-          setSnackbarSeverity("warning");
-          setSnackbarOpen(true);
-          props.setEvents(currEvents);
-        }
-      }).catch((error) => {
-        console.log(error);
-      });
+        .then((response) => {
+          if (response.status === 204) {
+            let index = currEvents.findIndex(e => e.id === event.id);
+            currEvents = [
+              ...currEvents.slice(0, index),
+              ...currEvents.slice(index + 1)
+            ];
+            setSnackbarMessage("Event deleted!");
+            setSnackbarSeverity("warning");
+            setSnackbarOpen(true);
+            props.setEvents(currEvents);
+          }
+        }).catch((error) => console.log(error));
   }
 
   function saveEventChanges() {
@@ -64,22 +69,38 @@ function EventDisplayCard(props) {
       description: description,
       date: epochTime,
       location: location,
-    }
+    };
 
     request("PUT", `/events/${getUserID()}`, newEvent)
-    .then((response) => {
-      let index = currEvents.findIndex(e => e.id === newEvent.id);
-      if( index !== -1) {
-        currEvents[index] = response.data;
-        props.setEvents(currEvents);
-      }
-      setSnackbarMessage("Event updated!");
-      setSnackbarSeverity("success");
-      setSnackbarOpen(true);
-    }).catch((error) => {
-      console.log(error);
-    });
+        .then((response) => {
+          let index = currEvents.findIndex(e => e.id === newEvent.id);
+          if (index !== -1) {
+            currEvents[index] = response.data;
+            props.setEvents(currEvents);
+          }
+          setSnackbarMessage("Event updated!");
+          setSnackbarSeverity("success");
+          setSnackbarOpen(true);
+        })
+        .catch((error) => console.log(error));
     setIsEditing(false);
+  }
+
+  function handleAddPet() {
+    if (!selectedPetId) {
+      setSnackbarMessage("Please select a pet.");
+      setSnackbarSeverity("warning");
+      setSnackbarOpen(true);
+      return;
+    }
+
+    useEffect(() => {
+      request("GET", `/pets/${getUserID()}`, null)
+          .then((response) => setPets(response.data))
+          .catch((error) => console.log(error));
+    }, []);
+
+    handleClose();
   }
 
   function onChangeName(e) {
@@ -89,7 +110,7 @@ function EventDisplayCard(props) {
   function onChangeDescription(e) {
     setDescription(e.target.value);
   }
-  
+
   function onChangeLocation(e) {
     setLocation(e.target.value);
   }
@@ -99,93 +120,114 @@ function EventDisplayCard(props) {
   }
 
   return (
-    <Card elevation={3} >
-      <CardContent component="form">
-        <Typography variant="h5" component="div">
-          Event name:{" "}
-          {isEditing ? (
+      <Card elevation={3}>
+        <CardContent component="form">
+          <Typography variant="h5" component="div">
+            Event name:{" "}
+            {isEditing ? (
                 <input
-                  value={name}
-                  onChange={onChangeName}
+                    value={name}
+                    onChange={onChangeName}
                 />
-              ) : (
+            ) : (
                 <span>{name}</span>
+            )}
+          </Typography>
+
+          <Typography>
+            <label>
+              Description:{" "}
+              {isEditing ? (
+                  <textarea
+                      rows={4}
+                      value={description}
+                      onChange={onChangeDescription}
+                  />
+              ) : (
+                  <span>{description}</span>
               )}
-        </Typography>
-        <Typography>
-          <label>
-            Description:{" "} 
+            </label>
+          </Typography>
+
+          <Typography>Date:{" "}
             {isEditing ? (
-              <textarea
-                rows={4} 
-                value={description}
-                onChange={onChangeDescription}  
-              />
+                <DateTimePicker
+                    value={date}
+                    onChange={(newValue) => setDate(newValue)}
+                />
             ) : (
-              <span>{description}</span>
+                <span>{date.toString()}</span>
             )}
+          </Typography>
 
-          </label>
-        </Typography>
+          <Typography>
+            <label>
+              Location:{" "}
+              {isEditing ? (
+                  <input
+                      value={location}
+                      onChange={onChangeLocation}
+                  />
+              ) : (
+                  <span>{location}</span>
+              )}
+            </label>
+          </Typography>
 
-        <Typography>Date:{" "} 
-        {isEditing ? (
-              <DateTimePicker 
-                value={date}
-                onChange={(newValue) => setDate(newValue)}
-              />
-            ) : (
-              <span>{date.toString()}</span>
-            )}
-        </Typography>
-        
-        <Typography>
-          <label>
-            Location:{" "} 
-            {isEditing ? (
-              <input
-                value={location}
-                onChange={onChangeLocation}  
-              />
-            ) : (
-              <span>{location}</span>
-            )}
+          {user.role === "ADOPTION_CENTER" && (
+              <>
+                {isEditing ? (
+                    <>
+                      <Button variant="outlined" onClick={saveEventChanges}>Confirm</Button>
+                      <Button variant="outlined" onClick={cancelEdit}>Cancel</Button>
+                    </>
+                ) : (
+                    <>
+                      <Button variant="outlined" onClick={handleEdit}>Edit</Button>
+                      <Button variant="outlined" onClick={handleRemove}>Remove</Button>
+                    </>
+                )}
+                <Typography>Add a Pet to this Event:</Typography>
+                <select defaultValue="" onChange={(e) => console.log(e.target.value)}>
+                  <option value="" disabled>
+                    Select a Pet
+                  </option>
+                  {pets.length > 0 ? (
+                      pets.map((pet) => (
+                          <option key={pet.id} value={pet.id}>
+                            {pet.name}
+                          </option>
+                      ))
+                  ) : (
+                      <option disabled>No Pets Available</option>
+                  )}
+                </select>
+                <Button variant="outlined" onClick={handleAddPet}>
+                  Add Pet
+                </Button>
+              </>
+          )}
+        </CardContent>
 
-          </label>
-        </Typography>
-        {console.log(props.user)}
-        { user.role === "ADOPTION_CENTER" && (isEditing ? 
-        <>
-          <Button variant="outlined" onClick={saveEventChanges}>Confirm</Button>
-          <Button variant="outlined" onClick={cancelEdit}>Cancel</Button>
-        </>
-        :
-        <>
-          <Button variant="outlined" onClick={handleEdit}>Edit</Button>
-          <Button variant="outlined" onClick={handleRemove}>Remove</Button>
-        </>) }
-      </CardContent>
-      <SnackbarNoti
+        <SnackbarNoti
             open={snackbarOpen}
             severity={snackbarSeverity}
             message={snackbarMessage}
             onClose={handleSnackbarClose}
-      />
-    </Card>
-  )
+        />
+      </Card>
+  );
 }
 
 export default function EventDisplay(props) {
-  const eventsCopy  = [...props.events];
+  const eventsCopy = [...props.events];
   return (
-    <Grid2 container spacing={2} paddingBottom={2} paddingLeft={2} paddingRight={2}>
-      {props.events.map((event) => {
-        return (
-          <Grid2 key={event.id} size={{xs: 12, sm: 6 }}>
-            <EventDisplayCard event={event} currEvents={eventsCopy} setEvents={props.setEvents} user={props.user}/>
-          </Grid2>
-        );
-      }) }
-    </Grid2>
-  )
+      <Grid2 container spacing={2} paddingBottom={2} paddingLeft={2} paddingRight={2}>
+        {props.events.map((event) => (
+            <Grid2 key={event.id} size={{ xs: 12, sm: 6 }}>
+              <EventDisplayCard event={event} currEvents={eventsCopy} setEvents={props.setEvents} user={props.user} />
+            </Grid2>
+        ))}
+      </Grid2>
+  );
 }
